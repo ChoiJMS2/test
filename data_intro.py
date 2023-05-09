b41c58f2226b9620215b848758d4c4c9b998ef46
ChoiJMS2 <cjm881209@gmail.com>
2023-05-09 16:01:58 +0900

    update

--- data_intro.py
+++ data_intro.py
@@ -20,20 +20,20 @@ def run_dataIntro():
                 "- 	Store metadata, including **city**, **state**, **type**, and **cluster**. \n"
                 "- **cluster** is a grouping of similar stores \n")
     img2 = Image.open("img/cb_stores.PNG")
-    st.image(img2, width=700, height=500)
+    st.image(img2)
     st.markdown("#### :clipboard: oil.csv \n"
                 "- 	Daily oil price. Includes values during both the train and test data timeframes. (Ecuador is an oil-dependent country and it's economical health is highly vulnerable to shocks in oil prices.) \n")
     img3 = Image.open("img/cb_oil.PNG")
-    st.image(img3, width=700, height=500)
+    st.image(img3)
     st.markdown("#### :clipboard: holidays_events.csv \n"
                 "- 	NOTE: Pay special attention to the **transferred** column. A holiday that is transferred officially falls on that calendar day, but was moved to another date by the government. A transferred day is more like a normal day than a holiday. To find the day that it was actually celebrated, look for the corresponding row where type is Transfer. For example, the holiday Independencia de Guayaquil was transferred from 2012-10-09 to 2012-10-12, which means it was celebrated on 2012-10-12. Days that are type Bridge are extra days that are added to a holiday (e.g., to extend the break across a long weekend). These are frequently made up by the type Work Day which is a day not normally scheduled for work (e.g., Saturday) that is meant to payback the Bridge. \n"
                 "- Additional holidays are days added a regular calendar holiday, for example, as typically happens around Christmas (making Christmas Eve a holiday). \n")
     img4 = Image.open("img/cb_holidays.PNG")
-    st.image(img4, width=700, height=500)
+    st.image(img4)
     st.markdown("#### :clipboard: transactions.csv \n"
                 "- Show the daily number of visitors to each store \n")
     img5 = Image.open("img/cb_transactions.PNG")
-    st.image(img5, width=700, height=500)
+    st.image(img5)
     st.markdown("###  :exclamation: Additional Notes \n"
                 "- Wages in the public sector are paid every two weeks on the 15 th and on the last day of the month. Supermarket sales could be affected by this."
                 "- A magnitude 7.8 earthquake struck Ecuador on April 16, 2016. People rallied in relief efforts donating water and other first need products which greatly affected supermarket sales for several weeks after the earthquake."
